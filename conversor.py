9c63ec84032ecee52faa4f2c4f3b7670d4e6da87
Luis Felipe Neto <lfelipeabreu9@gmail.com>
2026-05-26 15:05:44 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,5 @@
+real = float(input("Quanto você quer converter?: "))
+dolar = float(input("Quanto está a cotação do dólar para o real?: "))
+
+resultado = real/dolar
+print("Você terá ", resultado, "dólares.")
